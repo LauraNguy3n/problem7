ab9cd9549d1a19f54e3bda5d839ac1f66eb0b997
laura <you@example.com>
2024-08-21 12:27:04 -0500

    hi

---
+++ problem7/src/problem7/P7.java
@@ -0,0 +1,25 @@
+package problem7;
+import java.util.Scanner;
+public class P7 {
+
+	public static void main(String[] args) {
+		// For loop and array. calculate the sum in an array using for loop with user input
+		
+		System.out.println("type four numbers");
+		Scanner sc = new Scanner (System.in);
+		int q = sc.nextInt();
+		int sum = 0;
+	
+			int[]w= ;{
+
+		for(int q = 0; q < w.length; q++) {
+
+				sum += w[q];
+	
+			}
+			
+		System.out.println(sum);
+	
+		}
+			}
+				}
