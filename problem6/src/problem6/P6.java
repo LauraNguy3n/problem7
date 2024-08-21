cf5d3195b502930ae8865759e51f8a9f339fa6c3
laura <you@example.com>
2024-08-21 12:23:50 -0500

    this is actually problem 6

---
+++ problem6/src/problem6/P6.java
@@ -0,0 +1,23 @@
+package problem6;
+import java.util.Scanner;
+import java.util.Arrays;
+public class P6 {
+
+	public static void main(String[] args) {
+		// Array
+		
+		System.out.println("type random numbers");
+		Scanner sc=new Scanner(System.in);
+		int k=sc.nextInt();
+		
+		int [] numbers =  ;
+		
+		Arrays.sort (numbers);
+		
+		System.out.println(Arrays.toString(numbers));
+		
+		
+
+	}
+
+}
